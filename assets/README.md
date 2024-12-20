db729a8962963b3b95ec7233f0a496899298f21a
Denis Zholob <dddgamer3x@gmail.com>
2024-12-20 10:48:37 -0700

    Add animated sprite rotation

--- assets/README.md
+++ assets/README.md
@@ -1,3 +1,8 @@
 # Asset Credits
 
-* https://pixelfrog-assets.itch.io/tiny-swords
+* House: https://pixelfrog-assets.itch.io/tiny-swords
+* Eye: https://kenney.nl/assets/googly-eyes
+
+Modification tools:
+* https://deniszholob.github.io/tileset-format-tool/
+* https://www.gimp.org/
--- views/demos.gd
+++ views/demos.gd
@@ -1,14 +1,17 @@
 extends Control
 
 @onready var button_demo_fsm = $ScrollContainer/VBoxContainer/ButtonDemoFSM
+@onready var button_demo_animated_sprite_rotation: Button = $ScrollContainer/VBoxContainer/ButtonDemoAnimatedSpriteRotation
 @onready var button_demo_trasparency_zone = $ScrollContainer/VBoxContainer/ButtonDemoTrasparencyZone
 
-const DEMO_FSM:String = "res://views/demo-fsm/demo-fsm.tscn"
-const DEMO_TRANSPARENCY_ZONE:String = "res://views/demo-transparency-zone/demo-transparency-zone.tscn"
+const DEMO_FSM: String = "res://views/demo-fsm/demo-fsm.tscn"
+const DEMO_ANIMATED_SPRITE_ROTATION: String = 'res://views/demo-animated-sprite-rotation/demo-animated-sprite-rotation.tscn'
+const DEMO_TRANSPARENCY_ZONE: String = "res://views/demo-transparency-zone/demo-transparency-zone.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	button_demo_fsm.pressed.connect(func(): _change_scene(DEMO_FSM))
+	button_demo_animated_sprite_rotation.pressed.connect(func(): _change_scene(DEMO_ANIMATED_SPRITE_ROTATION))
 	button_demo_trasparency_zone.pressed.connect(func(): _change_scene(DEMO_TRANSPARENCY_ZONE))
 	pass
 
